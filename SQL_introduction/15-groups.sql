7dbbec9aeff08f63b87d885327dadc8e7bb0b884
jennnn2206 <jennifer_briceno@anahuacmayab.edu.mx>
2026-01-22 14:59:50 -0600

    .

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,5 @@
 -- Display the times a score is repeated
-SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
+SELECT score, COUNT(score) AS number
+FROM second_table
+GROUP BY score
+ORDER BY score DESC;
