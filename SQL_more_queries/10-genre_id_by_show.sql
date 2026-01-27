25398c31fd6d88bf899670091f6d317adf8d8cb0
jennnn2206 <jennifer_briceno@anahuacmayab.edu.mx>
2026-01-27 15:00:39 -0600

    .

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- khg
-SELECT tv_shows.title, tv_show_genres.genre_id 
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows -- table a 
-INNER JOIN tv_shows_genres -- table b 
-ON tv_shows.id = tv_show_genres.show_id 
-ORDER BY tv_shows.title, tv_show_genres.genre_id ASC; 
+INNER JOIN tv_show_genres -- table b 
+    ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
