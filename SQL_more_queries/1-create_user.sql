747064647cf112783be37daa3e97ff65928ce5d1
jennnn2206 <jennifer_briceno@anahuacmayab.edu.mx>
2026-01-27 07:45:05 -0600

    .

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- creating a user id if not exist and password
-CREATE USER 'user_0d_1'@'localhost' IF NOT EXIST IDENTIFIED BY 'user_0d_1_pwd'; 
+CREATE USER IF NOT EXIST 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'; 
 -- Privileges of the user 
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
