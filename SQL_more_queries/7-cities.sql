ad5f50d75b3b93e7d33f81847b66a89915725b65
jennnn2206 <jennifer_briceno@anahuacmayab.edu.mx>
2026-01-27 09:03:20 -0600

    .

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -6,6 +6,4 @@ USE hbtn_0d_usa;
 -- state_id INT, can’t be null and must be a FOREIGN KEY that references to id of the states table
 -- name VARCHAR(256) can’t be null
 CREATE TABLE cities 
-(id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
-states_id INT NOT NULL, FOREIGN KEY (states_id) REFERENCES states(id),
-name VARCHAR(256) NOT NULL);
+(id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY, state_id INT NOT NULL, FOREIGN KEY (state_id) REFERENCES states(id), name VARCHAR(256) NOT NULL);
